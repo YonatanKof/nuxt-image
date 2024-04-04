f73bde76a586de126bda1f88ba5eb4c9661b9ad2
Yonatan Kof <kof@yonatankof.com>
2024-04-04 17:42:13 +0300

    init nuxt image

---
+++ components/SayHi.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div>
+		<NuxtImg src="/PhotoKof-1.jpg" sizes="100vw sm:50vw md:400px" format="webp" />
+	</div>
+</template>
+
+<style scoped>
+img {
+	max-width: 24rem;
+}
+</style>
